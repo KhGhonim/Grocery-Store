1f85003943383f728bcfb42efa719b2541851e89
Khaled <khaledonem89@gmail.com>
2024-06-02 20:13:19 +0300

    Skleton Mask Added

--- app/ProductCatagory/[id]/ProductByCatagoryCard.jsx
+++ app/ProductCatagory/[id]/ProductByCatagoryCard.jsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { useEffect, useState } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 import Image from "next/image";
+import Loading from "../../loading";
 
 export default function ProductByCatagoryCard({ params }) {
   const [Data, setData] = useState([]);
@@ -24,10 +25,45 @@ export default function ProductByCatagoryCard({ params }) {
       setData(fillteredData);
     };
 
+
+
     getData();
   }, []);
+  if (!Data || Data.length === 0) {
+    return (
+      <section className="bg-white dark:bg-gray-900">
+    <div className="container px-6 py-10 mx-auto animate-pulse">
+        <h1 className="w-48 h-2 mx-auto bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
 
-  console.log(Data);
+        <p className="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+        <p className="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg sm:w-80 dark:bg-gray-700"></p>
+
+        <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="w-full ">
+                <div className="w-full h-64 bg-gray-300 rounded-lg md:h-72 dark:bg-gray-600"></div>
+                
+                <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+
+            <div className="w-full ">
+                <div className="w-full h-64 bg-gray-300 rounded-lg md:h-72 dark:bg-gray-600"></div>
+                
+                <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+
+            <div className="w-full ">
+                <div className="w-full h-64 bg-gray-300 rounded-lg md:h-72 dark:bg-gray-600"></div>
+                
+                <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            </div>
+        </div>
+    </div>
+</section>
+    )
+}
 
   return (
     <div className="flex justify-around flex-wrap max-sm:items-center gap-2 h-full mb-5">
@@ -47,8 +83,8 @@ export default function ProductByCatagoryCard({ params }) {
             />
             <h1 className=" font-extrabold my-2">{item.name}</h1>
             <div className="flex justify-evenly items-center gap-4 flex-row text-center  w-full">
-              <h1 className=" font-extrabold">{item.price}</h1>
-              <h1 className=" font-bold ">{item.Fakeprice}</h1>
+              <h1 className=" font-extrabold">${item.price}</h1>
+              <h1 className=" font-normal line-through  ">${item.Fakeprice}</h1>
             </div>
 
             <button
