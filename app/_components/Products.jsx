d9fc5763f1b7cc5206abf486d1ec0bfb9bdc57dd
Khaled <khaledonem89@gmail.com>
2024-06-02 19:28:02 +0300

    Done

--- app/_components/Products.jsx
+++ app/_components/Products.jsx
@@ -6,6 +6,7 @@ export default function Products() {
     <div className="px-4 text-green-600 font-extrabold text-center  my-8  ">
       <h1 className=" text-2xl ">Our Poplar Products</h1>
       <Suspense fallback={<Loading />}>
+        
       <SharedCard />
       </Suspense>   
     </div>
